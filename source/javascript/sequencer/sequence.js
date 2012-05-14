6f2b69f072c4629eca632d27dba08e8241281324
Stuart Keith <stuartjkeith@gmail.com>
2012-05-13 22:44:06 -0500

    Sequence now triggers an event when it advances.

--- source/javascript/sequencer/sequence.js
+++ source/javascript/sequencer/sequence.js
@@ -17,6 +17,8 @@ define([
 			if (this.position >= this.length)
 				this.position = 0;
 
+			this.trigger("update", this.position);
+
 			if (this.instrumentManager) {
 				var notes = this.notes._columns[this.position++];
 
