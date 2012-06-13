3082d9dd7062ea6283e056ea65ede3ffbe527002
Stuart Keith <stuartjkeith@gmail.com>
2012-06-13 02:34:54 -0500

    SoundOutput volume is stored and retrieved from localStorage.

--- source/javascript/commands/initialize.js
+++ source/javascript/commands/initialize.js
@@ -1,5 +1,9 @@
 define(function () {
 	var initialize = function () {
+		var soundOutputVolume = localStorage.getItem("soundOutputVolume");
+
+		if (soundOutputVolume != null)
+			this.soundOutput.setVolume(soundOutputVolume);
 	};
 
 	return initialize;
--- source/javascript/commands/setSoundOutputVolume.js
+++ source/javascript/commands/setSoundOutputVolume.js
@@ -1,6 +1,8 @@
 define(function () {
 	var setSoundOutputVolume = function (args) {
 		this.soundOutput.setVolume(args.volume);
+
+		localStorage.setItem("soundOutputVolume", args.volume);
 	};
 
 	return setSoundOutputVolume;
