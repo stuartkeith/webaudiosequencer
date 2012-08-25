d15488097f26fd1c08e8879db017553dba789403
Stuart Keith <stuartjkeith@gmail.com>
2012-08-25 21:32:44 +1000

    Update jQuery and jQuery UI.

--- source/javascript/main.js
+++ source/javascript/main.js
@@ -1,7 +1,7 @@
 require.config({
 	paths: {
-		jquery: "libraries/jquery/jquery-1.7.2",
-		jqueryUI: "libraries/jquery/jquery-ui-1.8.21",
+		jquery: "libraries/jquery/jquery-1.8.0",
+		jqueryUI: "libraries/jquery/jquery-ui-1.8.23",
 		underscore: "libraries/underscore/underscore-1.3.3",
 		backbone: "libraries/backbone/backbone-0.9.2",
 		templates: "../templates",
