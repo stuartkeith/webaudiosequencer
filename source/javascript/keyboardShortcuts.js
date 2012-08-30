bbfdccdc2647ea49e17f15a827acd0c4490d77b1
Stuart Keith <stuartjkeith@gmail.com>
2012-08-30 20:51:18 +1000

    Refactor keyboardShortcuts.js.

--- source/javascript/keyboardShortcuts.js
+++ source/javascript/keyboardShortcuts.js
@@ -1,14 +1,18 @@
-define([
-	"jquery"
-], function ($) {
+define(function (require) {
+	var $ = require("jquery");
+
 	var keyboardShortcuts = function (eventBus) {
 		$(document.documentElement).keydown(function (event) {
+			// number keys 1 to 0
 			var index = event.which - 49;
 
-			if (index >= 0 && index <= 10)
+			if (index >= 0 && index <= 10) {
 				eventBus.trigger("selectTrack", {
 					trackIndex: index
 				});
+
+				return;
+			}
 		});
 	};
 
