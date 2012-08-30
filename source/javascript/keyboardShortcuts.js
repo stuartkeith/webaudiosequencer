30623e263adb46e84cc5168e8267303adc10b5a8
Stuart Keith <stuartjkeith@gmail.com>
2012-08-30 22:57:04 +1000

    Spacebar keyboard shortcut toggles sequencer playback.

--- source/javascript/keyboardShortcuts.js
+++ source/javascript/keyboardShortcuts.js
@@ -3,6 +3,15 @@ define(function (require) {
 
 	var keyboardShortcuts = function (eventBus) {
 		$(document.documentElement).keydown(function (event) {
+			// spacebar
+			if (event.which === 32) {
+				eventBus.trigger("toggleSequencer");
+
+				event.preventDefault();
+
+				return;
+			}
+
 			// number keys 1 to 0
 			var index = event.which - 49;
 
@@ -10,6 +19,8 @@ define(function (require) {
 				eventBus.trigger("selectTrack", {
 					trackIndex: index
 				});
+
+				event.preventDefault();
 
 				return;
 			}
