aaa42201ec8a7b30e67fc055fe109709c1dcfbeb
Stuart Keith <stuartjkeith@gmail.com>
2012-09-05 08:42:00 +1000

    PlaySoundAttribute command's 'deferred' argument is optional.

--- source/javascript/commands/playSoundAttributes.js
+++ source/javascript/commands/playSoundAttributes.js
@@ -3,21 +3,26 @@ define(function () {
 		var soundAttributes = args.soundAttributes;
 		var deferred = args.deferred;
 
-		deferred.notify("loading");
+		if (deferred)
+			deferred.notify("loading");
 
 		var loadDeferred = this.soundOutput.loadSoundURL(soundAttributes.sound_url);
 
-		loadDeferred.fail(function (error, data) {
-			deferred.reject(error, data);
-		});
+		if (deferred) {
+			loadDeferred.fail(function (error, data) {
+				deferred.reject(error, data);
+			});
+		}
 
 		loadDeferred.done(function (buffer) {
-			deferred.notify("playing");
+			if (deferred)
+				deferred.notify("playing");
 
 			this.soundOutput.playBuffer(buffer, 0, 1, 0, function () {
 				this.soundOutput.freeSoundURL(soundAttributes.sound_url);
 
-				deferred.resolve();
+				if (deferred)
+					deferred.resolve();
 			}.bind(this));
 		}.bind(this));
 	};
