5fea035437a781c5d0b39536c42ccf00f603f692
Stuart Keith <stuartjkeith@gmail.com>
2012-09-14 15:11:40 +1000

    The removeTrack command calculates and passes on the index of the trackModel being removed.

--- source/javascript/commands/removeTrack.js
+++ source/javascript/commands/removeTrack.js
@@ -1,6 +1,7 @@
 define(function () {
 	var removeTrack = function (args) {
-		var trackModel = args.trackModel;
+		var trackModel = args.trackModel,
+		    index = this.trackCollection.models.indexOf(trackModel);
 
 		this.trackCollection.remove(trackModel);
 
