c0d2260b0650c2edc4a1f3ada55ce0bb8bb52194
Stuart Keith <stuart@stuartkeith.com>
2013-04-29 00:58:59 +0100

    UpdateBPM command parses BPM and checks range, isNaN.

--- source/javascript/commands/updateBPM.js
+++ source/javascript/commands/updateBPM.js
@@ -1,6 +1,20 @@
-define(function () {
+define(function (require) {
+	var settings = require("settings");
+
 	var updateBPM = function (args) {
-		this.scheduler.setBPM(args.bpm);
+		var bpm = parseInt(args.bpm, 10);
+		var newBPM;
+
+		if (isNaN(bpm))
+			newBPM = settings.bpmDefault;
+		else if (bpm < settings.bpmMinimum)
+			newBPM = settings.bpmMinimum;
+		else if (bpm > settings.bpmMaximum)
+			newBPM = settings.bpmMaximum;
+		else
+			newBPM = bpm;
+
+		this.scheduler.setBPM(newBPM);
 	};
 
 	return updateBPM;
